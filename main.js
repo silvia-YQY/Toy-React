ca9c6214f967fd3894e860d000ccf99f80cc44e0
silvia-YQY <545067444@qq.com>
2020-11-02 23:21:39 +0800

    文件拆分

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-const a = 1
-for (let i of [1, 2, 3]) {
-  console.log(i);
+import { createElement, Component } from './toy-react'
+
+class MyComponent extends Component {
+  constructor() {
+    super()
+  }
+  setAttribute (name, value) {
+
+  }
+  appendChild () {
+
+  }
+  render () {
+    return <div></div>
+  }
 }
 
-// jsx 转译成 dom 节点
-function createElement (tagName, attributes, ...children) {
-  let e = document.createElement(tagName)
-  console.log('createElement', e, tagName, attributes, ...children);
-  for (let p in attributes) {
-    e.setAttribute(p, attributes[p]);
-  }
-  for (let child of children) {
-    if (typeof child === 'string') {
-      child = document.createTextNode(child);
-    }
-    e.appendChild(child);
-  }
-  return e;
-}
 
-document.body.appendChild(<div id="p" class="c">
+render(<MyComponent id="p" class="c">
   <div>324</div>
   <div></div>
   <div></div>
-</div>)
+</MyComponent>)
---
+++ toy-react.js
@@ -0,0 +1,50 @@
+class ElementWrapper {
+  constructor(type) {
+
+  }
+  setAttribute (name, value) {
+
+  }
+  appendChild () {
+
+  }
+
+}
+
+class TextWrapper {
+  constructor(content) {
+
+  }
+  setAttribute (name, value) {
+
+  }
+  appendChild () {
+
+  }
+}
+
+
+// jsx 转译成 dom 节点
+export function createElement (type, attributes, ...children) {
+  let e;
+  if (typeof type === "string") {
+    e = document.createElement(type);
+  } else {
+    e = new type;
+  }
+
+  for (let p in attributes) {
+    e.setAttribute(p, attributes[p]);
+  }
+  for (let child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
+    e.appendChild(child);
+  }
+  return e;
+}
+
+export function render () {
+
+}
