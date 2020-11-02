39f923fa8cb731856e2d8abd0c0dbd04150151ce
silvia-YQY <545067444@qq.com>
2020-11-02 23:08:28 +0800

    构造 createElement函数

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ for (let i of [1, 2, 3]) {
   console.log(i);
 }
 
-let b = <div />
+// jsx 转译成 dom 节点
+function createElement (tagName, attributes, ...children) {
+  let e = document.createElement(tagName)
+  console.log('createElement', e, tagName, attributes, ...children);
+  for (let p in attributes) {
+    e.setAttribute(p, attributes[p]);
+  }
+  for (let child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
+    e.appendChild(child);
+  }
+  return e;
+}
+
+document.body.appendChild(<div id="p" class="c">
+  <div>324</div>
+  <div></div>
+  <div></div>
+</div>)
