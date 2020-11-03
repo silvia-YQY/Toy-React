47d6c4bc51bb1bde227641d050e84b13b878eaa0
silvia-YQY <545067444@qq.com>
2020-11-03 20:51:58 +0800

    add ElementWrapper &       child = new TextWrapper(child);

--- toy-react.js
+++ toy-react.js
@@ -1,34 +1,47 @@
 class ElementWrapper {
   constructor(type) {
-
+    this.root = document.createElement(type);
   }
   setAttribute (name, value) {
-
+    this.root.setAttribute(name, value);
   }
-  appendChild () {
-
+  appendChild (component) {
+    this.root.appendChild(component.root);
   }
 
 }
 
 class TextWrapper {
   constructor(content) {
-
-  }
-  setAttribute (name, value) {
-
-  }
-  appendChild () {
-
+    this.root = document.createTextNode(type);
   }
 }
 
+export class Component {
+  constructor() {
+    this.props = Object.create(null);
+    this.children = [];
+    this._root = null;
+  }
+  setAttribute (name, value) {
+    this.props[name] = value;
+  }
+  appendChild (component) {
+    this.children.push(component);
+  }
+  get root () {
+    if (!this._root) {
+      this._root = this.render().root;
+    }
+    return this._root;
+  }
+}
 
 // jsx 转译成 dom 节点
 export function createElement (type, attributes, ...children) {
   let e;
   if (typeof type === "string") {
-    e = document.createElement(type);
+    e = new ElementWrapper(type);
   } else {
     e = new type;
   }
@@ -38,7 +51,7 @@ export function createElement (type, attributes, ...children) {
   }
   for (let child of children) {
     if (typeof child === 'string') {
-      child = document.createTextNode(child);
+      child = new TextWrapper(child);
     }
     e.appendChild(child);
   }
