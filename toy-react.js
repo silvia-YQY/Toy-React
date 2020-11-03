661140bfd606fffef031b055f8e36f99b4624e4b
silvia-YQY <545067444@qq.com>
2020-11-03 22:29:46 +0800

    支持重新渲染

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol("render to dom")
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type);
@@ -6,14 +8,24 @@ class ElementWrapper {
     this.root.setAttribute(name, value);
   }
   appendChild (component) {
-    this.root.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(this.root, this.root.childNodes.length);
+    range.setEnd(this.root, this.root.childNodes.length);
+    component[RENDER_TO_DOM](range);
   }
-
+  [RENDER_TO_DOM] (range) {
+    range.deleteContents();
+    range.insertNode(this.root);
+  }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content);
+  }
+  [RENDER_TO_DOM] (range) {
+    range.deleteContents();
+    range.insertNode(this.root);
   }
 }
 
@@ -28,6 +40,9 @@ export class Component {
   }
   appendChild (component) {
     this.children.push(component);
+  }
+  [RENDER_TO_DOM] (range) {
+    this.render()[RENDER_TO_DOM](range);
   }
   get root () {
     if (!this._root) {
@@ -67,5 +82,9 @@ export function createElement (type, attributes, ...children) {
 }
 
 export function render (component, parentElement) {
-  parentElement.appendChild(component.root);
+  let range = document.createRange();
+  range.setStart(parentElement, 0);
+  range.setEnd(parentElement, parentElement.childNodes.length);
+  range.deleteContents();
+  component[RENDER_TO_DOM](range);
 }
